a084a7e1e293f4b7466ff4a6af3b40afb68c7d19
murali <murali.e@sosaley.in>
2021-03-16 12:15:02 +0530

    Initial commit

---
+++ app/src/main/java/com/sosaley/hatsun/authentication/SignupActivity.java
@@ -0,0 +1,142 @@
+package com.sosaley.hatsun.authentication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.sosaley.hatsun.R;
+
+import static com.sosaley.hatsun.utils.AppConstant.REQ_CODE;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        signInButton=(SignInButton)findViewById(R.id.gmailSignup);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+
+        signInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.gmailSignup:
+                signIn();
+                break;
+
+            /*case R.id.gLogout:
+                signOut();
+                break;*/
+        }
+
+    }
+
+    private void signIn() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQ_CODE);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == REQ_CODE) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                System.out.println("NameAndEmail "+personName+" "+personEmail);
+                
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        signOut();
+                    }
+                },3000);
+
+            }
+
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+           // Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+
+            System.out.println("SignInResultFailedCode "+e.getStatusCode());
+
+        }
+    }
+
+    private void signOut() {
+
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful()){
+                            System.out.println("SuccessfullyLogout");
+                        }
+
+
+                    }
+                });
+
+    }
+
+}
---
+++ app/src/main/java/com/sosaley/hatsun/utils/AppConstant.java
@@ -0,0 +1,8 @@
+package com.sosaley.hatsun.utils;
+
+public class AppConstant {
+
+    public static final int REQ_CODE=9001;
+    public static final int CAMERA_ACCESS=5;
+
+}
